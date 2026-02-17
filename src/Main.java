d605e06fc2bac86df0a67dc893e165dc0bd3cf01
Melis PC <miquelangel.melis@gmail.com>
2026-02-17 19:29:29 +0100

    It input player name from players

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,39 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Board board = new Board();
 
+        System.out.print("Who is Player 1: ");
+        String playerNamePick = sc.next();
+
+        Player player1 = new Player(playerNamePick);
+
+        System.out.print("\nWho is Player 2: ");
+        playerNamePick = sc.next();
+
+        Player player2 = new Player(playerNamePick);
+
         int turnCount = 0;
 
         while(true){
+            int playerTurn = (turnCount++ % 2) + 1;
+            String playerName;
+
+            if (playerTurn == 1){
+                playerName = player1.getPlayerName();
+            }
+            else{
+                playerName = player2.getPlayerName();
+            }
+
             System.out.println(board.boardToString());
 
-            int playerTurn = (turnCount++ % 2) + 1;
-            System.out.printf("\n(Player %d) Select a column (1 - 7): ", playerTurn);
+
+            System.out.printf("\n(%s) Select a column (1 - 7): ", playerName);
 
             int indexOfColumnPick = sc.nextInt() - 1;
 
             board.updateBoardOnColumn(indexOfColumnPick, new Coin(playerTurn));
         }
 
-        sc.close();
+        //sc.close();
     }
 }
